b26152ca034e6a8a71db24f6076f6c0386d2fd0a
WP <2381553155@qq.com>
2015-07-19 15:53:22 +0800

    2015-7-19 15:53 添加项目
    2015-7-19 15:53 添加项目

---
+++ ParseJSONForWPF/HttpRequest.cs
@@ -0,0 +1,119 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Windows.Threading;
+
+namespace ParseJSONForWPF
+{
+    class HttpRequest
+    {
+        MainWindow mainWindow = new MainWindow();
+        public HttpRequest(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+        }
+
+        /// <summary>
+        /// 图灵机器人的key值
+        /// </summary>
+        private string[] key = { "2b15219bcc2fc0c93e6beab1719da82a", 
+                                 "2f6ad3d1ab8d7ea7be90987674ab0f3c" 
+                               };
+        /// <summary>
+        /// 访问图灵机器人的url
+        /// </summary>
+        string url;
+
+        private string urlHelp(string key, string info)
+        {
+            url = "http://www.tuling123.com/openapi/api?key=" + key + "&info=" + info;
+            return url;
+        }
+
+        public void getTuRingReply(string info)
+        {
+            url = this.urlHelp("2b15219bcc2fc0c93e6beab1719da82a", info);
+            this.DoHttpWebRequest(url);
+        }
+
+        private void DoHttpWebRequest(string url)
+        {
+
+            //创建WebRequest类
+            WebRequest request = HttpWebRequest.Create(url);
+
+
+            //返回异步操作的状态
+            IAsyncResult result = (IAsyncResult)request.BeginGetResponse(ResponseCallback, request);
+
+        }
+
+        private void ResponseCallback(IAsyncResult result)
+        {
+            //获取异步操作返回的的信息
+            HttpWebRequest request = (HttpWebRequest)result.AsyncState;
+
+            //结束对 Internet 资源的异步请求
+            WebResponse response = request.EndGetResponse(result);
+
+            try
+            {
+                Stream stream = response.GetResponseStream();
+
+                Encoding ec = Encoding.UTF8;
+
+                StreamReader reader = new StreamReader(stream, ec);
+                string tuRingResponse = reader.ReadToEnd();//读取返回的json数据
+
+                Console.WriteLine(tuRingResponse);
+                Console.WriteLine("序列化:");
+
+                string code = tuRingResponse.Substring(8, 6);
+               // Console.WriteLine(code);
+
+                //文字类回复实体类对象
+                TextResponse txtrs = null;
+
+                ////列车类回复实体类对象
+                //TrainResponse trrs = null;
+
+                ////航班类回复实体类对象
+                //FlightResponse flrs = null;
+
+
+                //Callout peoplecallout = new Callout();
+                //文字类回复
+                if (code == "100000")
+                {
+                    txtrs = this.DeserializeFromJson<TextResponse>(tuRingResponse);
+
+                    this.mainWindow.updateContent(txtrs);
+                  
+                    //Console.WriteLine(txtrs.Code);
+                    //this.mainWindow.Dispatcher.Invoke(new Action(() => { this.mainWindow.responseContent_Text.Text = txtrs.Text;
+                    //}));
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public T DeserializeFromJson<T>(string jsonString) where T : class
+        {
+            T obj = JsonConvert.DeserializeObject<T>(jsonString);
+            return obj;
+        }
+
+    }
+}
---
+++ ParseJSONForWPF/TextResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseJSONForWPF
+{
+    public class TextResponse
+    {
+        /// <summary>
+        /// 状态码
+        /// </summary>
+        private int code;
+
+        public int Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+
+        /// <summary>
+        /// 文字内容
+        /// </summary>
+        private string text;
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+    }
+}
